daac5c36d34213fa0d71bf4e787a7ed6bb915873
chankruze <chankruze@gmail.com>
2024-09-01 18:59:47 +0530

    fix(css): add fonts and refactor tailwind config
    Signed-off-by: chankruze <chankruze@gmail.com>

--- app/root.tsx
+++ app/root.tsx
@@ -1,3 +1,4 @@
+import { LinksFunction } from "@remix-run/node";
 import {
   Links,
   Meta,
@@ -6,6 +7,13 @@ import {
   ScrollRestoration,
 } from "@remix-run/react";
 import "~/tailwind.css";
+
+export const links: LinksFunction = () => [
+  {
+    rel: "stylesheet",
+    href: "https://fonts.googleapis.com/css2?family=Montserrat:wght@400;500;700;800;900&family=Outfit:wght@400;500;600;700;900&family=Poppins:ital,wght@0,200;0,400;0,500;0,700;1,100&family=Roboto+Mono&family=Roboto:wght@400;500;700;900&display=swap",
+  },
+];
 
 export function Layout({ children }: { children: React.ReactNode }) {
   return (
